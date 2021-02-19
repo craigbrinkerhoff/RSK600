79f4fa54ad3a9718da47f395cd2215a762c84925
cbrinkerhoff_umass_edu <cbrinkerhoff_umass_edu@node26.maas>
2021-02-19 22:09:56 +0000

    rerun study

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#Rscript scripts/k600_analysis.R
-#Rscript scripts/MonteCarlo_analysis.R
-#Rscript scripts/runBIKER.R
-#Rscript scripts/validation.R
+Rscript scripts/k600_analysis.R
+Rscript scripts/MonteCarlo_analysis.R
+Rscript scripts/runBIKER.R
+Rscript scripts/validation.R
 Rscript scripts/FCO2_analysis.R
--- scripts/inputs.R
+++ scripts/inputs.R
@@ -26,8 +26,8 @@ cores <- 5 #num cores to run parallel processes on (depends on HPC session setup
 #Constants
 ###############
 g <- 9.8 #gravitational acceleration [m/s2]
-molarMass <- 44.01 #g/mol for CO2
-pCO2_a <- 390 #uatm
+molarMass <- 44.01 #[g/mol] for CO2
+pCO2_a <- 390 #[uatm]
 mannings_uncertainity <- 0.25 #see MonteCarlo_analysis.R for actual estimated uncertainty
   #For BIKER va.idation, should assume only error is from manning's equation (0.25), NOT k600 model b/c we are assuming this model is 'true'
   #In actual implementation, we want posterior uncertainty to reflect both
@@ -37,4 +37,4 @@ mannings_uncertainity <- 0.25 #see MonteCarlo_analysis.R for actual estimated un
 ###############
 M <- 5000 # number of sets of hydraulic 'measurements'
 munge <- 0 #only set to 1 to grab M hydraulic measurements. Already done so leave at 0
-munge2 <- 0 #Set to 1 to run parallelized MC analyses. These take like a LONG time, even on an HPC so leave off if possible...
+munge2 <- 1 #Set to 1 to run parallelized MC analyses. These really need to be run on an HPC. This is by far the most compute heavy portion of the project

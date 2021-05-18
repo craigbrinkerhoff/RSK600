f6af400ceeb87382f011ed41454f482be6612deb
Craig Brinkerhoff <cbrinkerhoff@umass.edu>
2021-05-18 17:09:06 -0400

    first rewrite of methods using ustar model

--- generateMS.R
+++ generateMS.R
@@ -2,9 +2,11 @@
 #This will generate and store all results in your current RStudio environment so that you can knit the manuscript correctly, and then it'' knit the manuscript too.
 #################
 
+setwd('C:\\Users\\craig\\Documents\\GitHub\\RSK600')
+
 #Move all results to global environment
-results_3_1 <- read.csv('cache//k600//results.csv')
-results_3_1_MC <- read.csv('cache//MonteCarlo//results.csv')
+#results_3_1 <- read.csv('cache//k600//results.csv')
+#results_3_1_MC <- read.csv('cache//MonteCarlo//results.csv')
 results_3_2_rivs <- read.csv('cache//validation//results_by_riv.csv')
 results_3_2_all <- read.csv('cache//validation//results_all_riv.csv')
 results_3_3_all <- read.csv('cache//FCO2//fco2_stats_all.csv')
@@ -13,7 +15,7 @@ results_3_3_bulk <- read.csv('cache//FCO2//bulkFluxes.csv')
 results_3_3_bulk$key <- as.character(results_3_3_bulk$key)
 
 #knit manuscript
-rmarkdown::render("docs//manuscript_v2.Rmd")
+rmarkdown::render("manuscript_v3.Rmd")
 print('Manuscript generated.')
 
 rmarkdown::render("docs//supp_v1.Rmd")
